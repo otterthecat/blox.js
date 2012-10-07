c7135e8acd35adac29da51e28dc62eff51f5d8ce
Otter <otc@otterthecat.com>
2012-10-07 16:14:48 -0500

    Initial first pass commit of blox.js

--- README.md
+++ README.md
@@ -2,3 +2,75 @@ blox.js
 =======
 
 A way to modularize javascript code
+
+caveats
+-------
+
+This is still a work in progress - and while I don't intdend any of the major features/functions to change
+anytime soon, it's entirely possible there will be changes. 
+
+Documentation is also not complete, in part because this is still in progress, and in part because I'm
+lazy.
+
+To summarize: 'blah blah blah. Use at your own risk'
+
+instantiation
+-------------
+
+Create a new blox by invoking the builder function:
+
+	'blox = buildBlox(BLOX, options)'
+
+currently the only available options is *devMode*, whcih defaults to *true*
+
+
+basic usage
+-----------
+
+There are essentially two primary ways to work with a *blox* object: usinng *add* or *subscribe/publish*.
+
+### add
+
+Below is a typical usage scenario. When invoking the *add* method, pass a new object with a namepace defined
+(which will identify the function if it needs to be called directly), the actual function to run (within the 'fn')
+property, and the (currently optional) *assert* property, which will act as a simple unit test if *devMode* is enabled.
+The assertions can be viewed from a browsers console (if supported).
+
+	blox.add({
+		namespace: 'do_this',
+		fn: function(){
+
+			return document.getElementsByTagName('body')[0].style.color = 'red';
+		},
+		assert: function(){
+
+			return document.getElementsByTagName('body')[0].style.color;
+		}
+
+	});
+
+All functions added in this manner can then be called using 
+
+	blox.launch();
+
+which will wait for DOM ready or page load before running (depending on browser).
+
+### subscribe/publish
+
+Additionally, a *blox* can be used as a basic *pub/sub* by simply passing a defined event and desired
+as arguments:
+
+	blox.subscribe("drink", function(){
+
+		alert("burp");
+	});
+
+	blox.subscribe("drink", function(data){
+
+		alert("I think I'm drunk on "+data);
+	});
+
+The two subscribers above could then be called with a *blox*'s *publish* method,
+passing data to the subscribers via the *publish* method's second argument:
+
+	blox.publish("drink", "root beer");
---
+++ blox.js
@@ -0,0 +1,310 @@
+var BLOX = {
+
+	// global window object
+	win: window,
+	
+	// global document
+	doc: document,
+	
+	// object to hold user functions
+	funcs: {},
+	
+	// object to hold arguments for same-namespaced "funcs" (optional)
+	args: {},
+	
+	// object to hold variables to be shared across funcs
+	vars: {},
+	
+	// default settings (user configurable)
+	config: {
+		devMode: true,
+		preventVarOverride: true
+	},
+	
+	// object to hold susbscribing events
+	subscribers: {},
+	
+	// array of of unit testing functions (ignored if config.devMode set to true)
+	testables: [],
+
+	// collection of helper methods
+	utils: {
+
+		merge: function(baseObj, addObj){
+
+			for(item in addObj){
+
+				// 'merge' cannot be overridden in this manner,
+				// as it is used elsewhere in 'blox_prototype'
+				if(item !== 'merge'){
+					baseObj[item] = addObj[item];
+				} else {
+
+					BLOX.dbug('warn', '[merge] is not replaceable in blox_prototype, ignoring %o', item);
+				}
+			}
+
+			return baseObj;
+
+		},
+
+		// used for simple unit testing of 'funcs' functions
+		assert: function(obj) {
+
+			// sample expected format:
+			// {namespace : '', testValue : '', assertValue : ''}
+			if(!BLOX.config.devMode) {
+				return null;
+			}
+
+			BLOX.dbug('warn', 'Assert [' + obj.namespace + ']: ' + obj.testValue + " === " + obj.assertValue);
+
+			if(obj.testValue === obj.assertValue) {
+
+				BLOX.dbug('info', "PASS : " + obj.testValue + " === " + obj.assertValue);
+				return true;
+			} else {
+
+				BLOX.dbug('error', "FAIL : " + obj.testValue + " !== " + obj.assertValue);
+				return false;
+			}
+		}
+	},
+
+
+	setUtils: function(utilObj){
+
+		utils = this.utils;
+		utils.merge(utils, utilObj);
+
+		return this;
+	},
+
+	// user configurations can be passed
+	init: function(settings) {
+
+		this.utils.merge(this.config, settings);
+
+		return this;
+	},
+
+	// fire 'blox' to happen on dom load
+	// (or page load for lesser browsers)
+	launch: function() {
+
+		var blox = this;
+
+		// browser is not inept
+		if(blox.doc.addEventListener) {
+
+			blox.doc.addEventListener("DOMContentLoaded", function() {
+
+				return blox.exec();
+
+			}, false);
+
+		} else {
+			// browser is inept explorer 8 or less
+			blox.win.onload = function() {
+
+				return blox.exec();
+			};
+		}
+	},
+
+	// runs through 'funcs' object and call all functions within.
+	// will run in tandem with utils.assert() if config.devMode set to true
+	exec: function(namespace) {
+
+		blox = this;
+
+		// no namespace passed, so loop through them all
+		if(!namespace) {
+
+
+			if(!blox.config.devMode) {
+				for(var item in blox.funcs) {
+					(blox.args.hasOwnProperty(item)) ? blox.funcs[item](blox.args[item]) : blox.funcs[item]();
+
+				}
+			}
+
+			// TODO tidy this block up a bit
+			if(blox.config.devMode) {
+
+				var t = blox.testables;
+
+				for(var i = 0; i < t.length; i++) {
+
+					blox.utils.assert({
+						namespace: t[i].namespace,
+						testValue: t[i].fn(),
+						assertValue: typeof(t[i].assert) === 'function' ? t[i].assert() : t[i].assert
+					});
+
+				}
+			}
+
+			return blox;
+
+		// namespace is passed, and we found a matching function, so call it
+		} else if(blox.funcs.hasOwnProperty(namespace)) {
+
+			(blox.args.hasOwnProperty(namespace)) ? blox.funcs[namespace](blox.args[namespace]) : blox.funcs[namespace]();
+
+			return blox;
+
+			// no namespace matching a function - tell user we have no idea what they're
+			// trying to do.
+		} else {
+
+			blox.dbug('warn', 'blox does not have requested namespace [%s].', namespace);
+			return false;
+		}
+	},
+
+	// where user created functions occur
+	// i.e. - update 'funcs' object, and optionally
+	// 'args' and/or 'testables'
+	add: function(obj) {
+
+		blox = this;
+
+		var testables = blox.testables;
+		if(!blox.funcs.hasOwnProperty(obj.namespace) && obj.arg !== undefined) {
+			blox.funcs[obj.namespace] = obj.fn;
+			blox.args[obj.namespace] = obj.arg;
+
+			return blox;
+
+		// no arguments, and namespace does not already exist
+		// so only update the funcs object
+		} else if(!blox.funcs.hasOwnProperty(obj.namespace)) {
+			blox.funcs[obj.namespace] = obj.fn;
+
+			// TODO - ensure namespace can't be written over
+			testables.push({
+				namespace: obj.namespace,
+				fn: obj.fn,
+				assert: obj.assert
+			});
+
+			return blox;
+
+		// we already have that namespace - so warn the user and exit
+		} else {
+
+			blox.dbug("error", "blox already contains namespace [%s].", obj.namespace);
+			return false;
+		}
+
+		return blox;
+	},
+
+	// remove specified function from 'funcs'
+	remove: function(namespace) {
+
+		delete this.funcs[namespace]
+		delete this.args[namespace];
+
+	},
+
+	// note that the "pub/sub" feature is
+	// independent from 'funcs', or 'testables'
+	subscribe: function(ev, fn) {
+
+		if(!this.subscribers[ev]) {
+			this.subscribers[ev] = [];
+		}
+
+		this.subscribers[ev].push(fn);
+
+		return this.subscribers[ev];
+
+	},
+
+	// remove specified function from event subscription
+	unsubscribe: function(ev, fn) {
+
+		for(var i = 0; i < this.subscribers[ev].length; i++) {
+
+			if(this.subscribers[ev][i] === fn) {
+
+				this.subscribers[ev].splice(i, 1);
+			}
+		}
+
+		return this.subscribers[ev];
+
+	},
+
+	// publish event to subscribers with (optional) data
+	publish: function(ev, data) {
+		// add assert calls here as well?
+		for(var item in this.subscribers[ev]) {
+
+			data ? this.subscribers[ev][item](data) : this.subscribers[ev][item]();
+		}
+
+		return this;
+	},
+
+	// wrapper for firebug/webkit console - without breaking
+	// non supported browsers (guess which those are...)
+	dbug: function(lvl, msg, ob) {
+
+		blox = this;
+
+		// if not ie < 9.
+		if(!blox.win.console || !blox.config.devMode) {
+			return blox;
+		};
+
+		switch(lvl) {
+
+		case 'info':
+
+			ob ? blox.win.console.info(msg, ob) : blox.win.console.info(msg);
+			break;
+
+		case 'log':
+
+			ob ? blox.win.console.log(msg, ob) : blox.win.console.log(msg);
+			break;
+
+		case 'warn':
+
+			ob ? blox.win.console.warn(msg, ob) : blox.win.console.warn(msg);
+			break;
+
+		case 'error':
+
+			ob ? blox.win.console.error(msg, ob) : blox.win.console.error(msg);
+			console.log("+++ Start Stack Trace +++");
+			console.trace();
+			console.log("+++ End Stack Trace +++");
+			break;
+
+		default:
+
+			return blox.win.console.error('DOH! blox.dbug passed unavailable level [%s]. Try "info", "log", "warn" or "error"', lvl);
+		};
+
+		return blox;
+
+	}
+
+}
+
+// used for spawning new blox_prototype objects
+function buildBlox(proto, args) {
+
+	function F() {};
+	F.prototype = proto;
+
+	var f = new F();
+
+	f.init(args);
+
+	return f;
+}
