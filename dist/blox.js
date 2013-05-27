881e2aae02c4a9e0eb84b1d64508f56ed7fab457
Otter <otc@otterthecat.com>
2013-05-27 13:35:21 -0500

    Replaced tabs with spaces

--- dist/blox.js
+++ dist/blox.js
@@ -1,384 +1,387 @@
-/*! Blox JS - v0.1.0 - 2013-05-26
+/*! Blox JS - v0.1.0 - 2013-05-27
 * https://github.com/otterthecat/blox.js
 * Copyright (c) 2013 Otter the Cat; Licensed MIT, GPL */
 (function() {
 
-	// "private" method to load external scripts
-	// for any given block
-	var loadScript = function(script_path, callback_obj) {
+  // "private" method to load external scripts
+  // for any given block
+  var loadScript = function(script_path, callback_obj) {
 
-			var newScript = document.createElement('script');
-			newScript.type = 'text/javascript';
-			newScript.src = script_path;
+    var newScript = document.createElement('script');
+    newScript.type = 'text/javascript';
+    newScript.src = script_path;
 
-			newScript.onload = function() {
-				callback_obj.fn.call(callback_obj.scope, callback_obj.arg);
-			};
+    newScript.onload = function() {
 
-			document.getElementsByTagName('body')[0].appendChild(newScript);
+      callback_obj.fn.call(callback_obj.scope, callback_obj.arg);
+    };
 
-		};
+    document.getElementsByTagName('body')[0].appendChild(newScript);
+  };
 
-	var Blox = function(options){
+  var Blox = function(options){
 
-		// global doc
-		this.document = window.document;
+    // global doc
+    this.document = window.document;
 
-		// object to hold paths of imported scripts
-		this.includes = {};
+    // object to hold paths of imported scripts
+    this.includes = {};
 
-		// object to hold user functions
-		this.funcs = {};
+    // object to hold user functions
+    this.funcs = {};
 
-		// object to hold arguments for same-namespaced "funcs" (optional)
-		this.args = {};
+    // object to hold arguments for same-namespaced "funcs" (optional)
+    this.args = {};
 
-		// object to hold variables to be shared across funcs
-		this.vars = {};
+    // object to hold variables to be shared across funcs
+    this.vars = {};
 
-		// default settings (user configurable)
-		this.config = {
-			devMode: true,
-			scripts: [],
-			preventVarOverride: true
-		};
+    // default settings (user configurable)
+    this.config = {
+      
+      devMode: true,
+      scripts: [],
+      preventVarOverride: true
+    };
 
-		// object to hold susbscribing events
-		this.subscribers = {};
+    // object to hold susbscribing events
+    this.subscribers = {};
 
-		// array of of unit testing functions (ignored if config.devMode set to true)
-		this.testables = [];
+    // array of of unit testing functions (ignored if config.devMode set to true)
+    this.testables = [];
 
-		// collection of helper methods
-		this.utils = {
+    // collection of helper methods
+    this.utils = {
 
-			merge: function(baseObj, addObj) {
+      merge: function(baseObj, addObj) {
 
-				for(var item in addObj) {
+        for(var item in addObj) {
 
-					// 'merge' cannot be overridden in this manner,
-					// as it is used elsewhere in 'blox_prototype'
-					if(item !== 'merge') {
-						baseObj[item] = addObj[item];
-					} else {
+          // 'merge' cannot be overridden in this manner,
+          // as it is used elsewhere in 'blox_prototype'
+          if(item !== 'merge') {
 
-						this.dbug('warn', '[merge] is not replaceable in blox, ignoring %o', item);
-					}
-				}
+            baseObj[item] = addObj[item];
+          } else {
 
-				return baseObj;
+            this.dbug('warn', '[merge] is not replaceable in blox, ignoring %o', item);
+          }
+        }
 
-			},
+        return baseObj;
+      },
 
-			// used for simple unit testing of 'funcs' functions
-			assert: function(obj) {
+      // used for simple unit testing of 'funcs' functions
+      assert: function(obj) {
 
-				// sample expected format:
-				// {inc: '', namespace : '', testValue : '', assertValue : ''}
-				if(!this.config.devMode) {
-					return null;
-				}
+        // sample expected format:
+        // {inc: '', namespace : '', testValue : '', assertValue : ''}
+        if(!this.config.devMode) {
 
-				this.dbug('warn', 'Assert [' + obj.namespace + ']: ' + obj.testValue + " === " + obj.assertValue);
+          return null;
+        }
 
-				if(obj.testValue === obj.assertValue) {
+        this.dbug('warn', 'Assert [' + obj.namespace + ']: ' + obj.testValue + " === " + obj.assertValue);
 
-					this.dbug('info', "PASS : " + obj.testValue + " === " + obj.assertValue);
-					return true;
-				} else {
+        if(obj.testValue === obj.assertValue) {
 
-					this.dbug('error', "FAIL : " + obj.testValue + " !== " + obj.assertValue);
-					return false;
-				}
-			}
-		};
+          this.dbug('info', "PASS : " + obj.testValue + " === " + obj.assertValue);
 
-		this.init(options);
-	};
+          return true;
+        } else {
 
-	Blox.prototype = {
+          this.dbug('error', "FAIL : " + obj.testValue + " !== " + obj.assertValue);
+          return false;
+        }
+      }
+    };
 
-		// user configurations can be passed
-		init: function(settings) {
+    this.init(options);
+  };
 
-			if(typeof settings === 'object') {
+  Blox.prototype = {
 
-				this.utils.merge(this.config, settings);
-			}
+    // user configurations can be passed
+    init: function(settings) {
 
-			return this;
-		},
+      if(typeof settings === 'object') {
 
+          this.utils.merge(this.config, settings);
+      }
 
-		setUtils: function(utilObj) {
+      return this;
+    },
 
-			var utils = this.utils;
-			utils.merge(utils, utilObj);
 
-			return this;
-		},
+    setUtils: function(utilObj) {
 
-		// fire 'bblox' to happen on dom load
-		// (or page load for lesser browsers)
-		launch: function() {
+      var utils = this.utils;
+      utils.merge(utils, utilObj);
 
-			var _this = this;
-			// browser is not inept
-			if(this.document.addEventListener) {
+      return this;
+    },
 
-				this.document.addEventListener("DOMContentLoaded", function() {
+    // fire 'bblox' to happen on dom load
+    // (or page load for lesser browsers)
+    launch: function() {
 
-					return _this.exec();
+      var _this = this;
+      // browser is not inept
+      if(this.document.addEventListener) {
 
-				}, false);
+          this.document.addEventListener("DOMContentLoaded", function() {
 
-			} else {
-				// browser is inept explorer 8 or less
-				window.onload = function() {
+              return _this.exec();
 
-					return _this.exec();
-				};
-			}
-		},
+          }, false);
 
-		// runs through 'funcs' object and call all functions within.
-		// will run in tandem with utils.assert() if config.devMode set to true
-		exec: function(namespace) {
+      } else {
+          // browser is inept explorer 8 or less
+          window.onload = function() {
 
-			// no namespace passed, so loop through them all
-			if(!namespace) {
+              return _this.exec();
+          };
+      }
+    },
 
-				if(!this.config.devMode) {
+    // runs through 'funcs' object and call all functions within.
+    // will run in tandem with utils.assert() if config.devMode set to true
+    exec: function(namespace) {
 
-					for(var item in this.funcs) {
+      // no namespace passed, so loop through them all
+      if(!namespace) {
 
-						if(this.includes.hasOwnProperty(item) && typeof this.includes[item] === 'string') {
+          if(!this.config.devMode) {
 
-							var _this = this;
-							loadScript(this.includes[item], {
-								fn: _this.funcs[item],
-								scope: _this,
-								arg:  _this.args[item] || undefined
-							});
+              for(var item in this.funcs) {
 
-						} else {
+                  if(this.includes.hasOwnProperty(item) && typeof this.includes[item] === 'string') {
 
-							this.funcs[item].call(this, this.args[item] || undefined);
-						}
-					}
-				}
+                      var _this = this;
+                      loadScript(this.includes[item], {
+                          fn: _this.funcs[item],
+                          scope: _this,
+                          arg:  _this.args[item] || undefined
+                      });
 
-				// TODO tidy this block up a bit
-				if(this.config.devMode) {
+                  } else {
 
-					var t = this.testables;
+                      this.funcs[item].call(this, this.args[item] || undefined);
+                  }
+              }
+          }
 
-					for(var i = 0; i < t.length; i++) {
+          // TODO tidy this block up a bit
+          if(this.config.devMode) {
 
-						var _testable = t[i];
+              var t = this.testables;
 
-						if(typeof _testable.inc === 'string' && _testable.inc.length > 0) {
+              for(var i = 0; i < t.length; i++) {
 
-							loadScript(_testable.inc, {
+                  var _testable = t[i];
 
-								fn: this.utils.assert,
-								scope: this,
-								arg: {
-									namespace: _testable.namespace,
-									testValue: _testable.fn.call(this),
-									assertValue: typeof(_testable.assert) === 'function' ? _testable.assert() : _testable.assert
-								}
-							});
+                  if(typeof _testable.inc === 'string' && _testable.inc.length > 0) {
 
-						} else {
+                      loadScript(_testable.inc, {
 
-							this.utils.assert({
-								namespace: _testable.namespace,
-								testValue: _testable.fn.call(this),
-								assertValue: typeof(_testable.assert) === 'function' ? _testable.assert() : _testable.assert
-							});
-						}
-					}
-				}
+                          fn: this.utils.assert,
+                          scope: this,
+                          arg: {
+                              namespace: _testable.namespace,
+                              testValue: _testable.fn.call(this),
+                              assertValue: typeof(_testable.assert) === 'function' ? _testable.assert() : _testable.assert
+                          }
+                      });
 
-				return this;
+                  } else {
 
-				// namespace is passed, and we found a matching function, so call it
-			} else if(this.funcs.hasOwnProperty(namespace)) {
+                      this.utils.assert({
+                          namespace: _testable.namespace,
+                          testValue: _testable.fn.call(this),
+                          assertValue: typeof(_testable.assert) === 'function' ? _testable.assert() : _testable.assert
+                      });
+                  }
+              }
+          }
 
-				this.funcs[namespace](this.args[namespace] || undefined);
-				return this;
+          return this;
 
-				// no namespace matching a function - tell user we have no idea what they're
-				// trying to do.
-			} else {
+      // namespace is passed, and we found a matching function, so call it
+      } else if(this.funcs.hasOwnProperty(namespace)) {
 
-				this.dbug('warn', 'BLOX does not have requested namespace [%s].', namespace);
-				return false;
-			}
-		},
+        this.funcs[namespace](this.args[namespace] || undefined);
+        return this;
 
-		// get or set an internal variable.
-		// setter can be either with name/value strings
-		// or object literal with properties 'name' and 'value'
-		v: function(newVar, value) {
+      // no namespace matching a function - tell user we have no idea what they're
+      // trying to do.
+      } else {
 
-			if(typeof newVar === 'string' && typeof value !== 'undefined') {
+        this.dbug('warn', 'BLOX does not have requested namespace [%s].', namespace);
+        return false;
+      }
+    },
 
-				if(!this.vars.hasOwnProperty(newVar)) {
-					this.vars[newVar] = value;
-				}
+    // get or set an internal variable.
+    // setter can be either with name/value strings
+    // or object literal with properties 'name' and 'value'
+    v: function(newVar, value) {
 
-				return this;
+        if(typeof newVar === 'string' && typeof value !== 'undefined') {
 
-			} else if(typeof newVar === 'string' && typeof value === 'undefined') {
+          if(!this.vars.hasOwnProperty(newVar)) {
+              this.vars[newVar] = value;
+          }
 
-				return this.vars[newVar];
+          return this;
 
-			} else if(typeof newVar === 'object') {
+        } else if(typeof newVar === 'string' && typeof value === 'undefined') {
 
-				if(!this.vars.hasOwnProperty(newVar.name)) {
-					this.vars[newVar.name] = newVar.value;
-				}
+          return this.vars[newVar];
 
-				return this;
-			}
-		},
+        } else if(typeof newVar === 'object') {
 
-		// where user created functions occur
-		// i.e. - update 'funcs' object, and optionally
-		// 'args' and/or 'testables'
-		add: function(obj) {
+          if(!this.vars.hasOwnProperty(newVar.name)) {
+              this.vars[newVar.name] = newVar.value;
+          }
 
-			var testables = this.testables;
-			if(!this.funcs.hasOwnProperty(obj.namespace) && obj.arg !== undefined) {
+          return this;
+        }
+    },
 
-				this.funcs[obj.namespace] = obj.fn;
-				this.args[obj.namespace] = obj.arg;
-				this.includes[obj.namespace] = obj.inc;
+    // where user created functions occur
+    // i.e. - update 'funcs' object, and optionally
+    // 'args' and/or 'testables'
+    add: function(obj) {
 
-				return this;
+      var testables = this.testables;
+      if(!this.funcs.hasOwnProperty(obj.namespace) && obj.arg !== undefined) {
 
-				// no arguments, and namespace does not already exist
-				// so only update the funcs object
-			} else if(!this.funcs.hasOwnProperty(obj.namespace)) {
-				this.funcs[obj.namespace] = obj.fn;
-				this.includes[obj.namespace] = obj.inc;
+        this.funcs[obj.namespace] = obj.fn;
+        this.args[obj.namespace] = obj.arg;
+        this.includes[obj.namespace] = obj.inc;
 
-				// TODO - ensure namespace can't be written over
-				testables.push({
-					inc: obj.inc,
-					namespace: obj.namespace,
-					fn: obj.fn,
-					assert: obj.assert
-				});
+        return this;
 
-				return this;
+      // no arguments, and namespace does not already exist
+      // so only update the funcs object
+      } else if(!this.funcs.hasOwnProperty(obj.namespace)) {
+        this.funcs[obj.namespace] = obj.fn;
+        this.includes[obj.namespace] = obj.inc;
 
-				// we already have that namespace - so warn the user and exit
-			} else {
+        // TODO - ensure namespace can't be written over
+        testables.push({
+          inc: obj.inc,
+          namespace: obj.namespace,
+          fn: obj.fn,
+          assert: obj.assert
+        });
 
-				this.dbug("error", "BLOX already contains namespace [%s].", obj.namespace);
-				return false;
-			}
+        return this;
 
-			return this;
-		},
+      // we already have that namespace - so warn the user and exit
+      } else {
 
-		// remove specified function from 'funcs'
-		remove: function(namespace) {
+          this.dbug("error", "BLOX already contains namespace [%s].", obj.namespace);
+          return false;
+      }
 
-			delete this.funcs[namespace];
-			delete this.args[namespace];
+      return this;
+    },
 
-			return this;
-		},
+    // remove specified function from 'funcs'
+    remove: function(namespace) {
 
-		// note that the "pub/sub" feature is
-		// independent from 'funcs', or 'testables'
-		subscribe: function(ev, fn) {
+      delete this.funcs[namespace];
+      delete this.args[namespace];
 
-			if(!this.subscribers[ev]) {
-				this.subscribers[ev] = [];
-			}
+      return this;
+    },
 
-			this.subscribers[ev].push(fn);
+    // note that the "pub/sub" feature is
+    // independent from 'funcs', or 'testables'
+    subscribe: function(ev, fn) {
 
-			return this;
+      if(!this.subscribers[ev]) {
 
-		},
+        this.subscribers[ev] = [];
+      }
 
-		// remove specified function from event subscription
-		unsubscribe: function(ev, fn) {
+      this.subscribers[ev].push(fn);
 
-			for(var i = 0; i < this.subscribers[ev].length; i++) {
+      return this;
+    },
 
-				if(this.subscribers[ev][i] === fn) {
+    // remove specified function from event subscription
+    unsubscribe: function(ev, fn) {
 
-					this.subscribers[ev].splice(i, 1);
-				}
-			}
+      for(var i = 0; i < this.subscribers[ev].length; i++) {
 
-			return this;
+        if(this.subscribers[ev][i] === fn) {
 
-		},
+          this.subscribers[ev].splice(i, 1);
+        }
+      }
 
-		// publish event to subscribers with (optional) data
-		publish: function(ev, data) {
-			// add assert calls here as well?
-			for(var item in this.subscribers[ev]) {
+      return this;
+    },
 
-				this.subscribers[ev][item](data || undefined);
-			}
+    // publish event to subscribers with (optional) data
+    publish: function(ev, data) {
+        // add assert calls here as well?
+        for(var item in this.subscribers[ev]) {
 
-			return this;
-		},
+          this.subscribers[ev][item](data || undefined);
+        }
 
-		// wrapper for firebug/webkit console - without breaking
-		// non supported browsers (guess which those are...)
-		dbug: function(lvl, msg, ob) {
+        return this;
+    },
 
-			// if not ie < 9.
-			if(!window.console || !this.config.devMode) {
-				return this;
-			}
+    // wrapper for firebug/webkit console - without breaking
+    // non supported browsers (guess which those are...)
+    dbug: function(lvl, msg, ob) {
 
-			switch(lvl) {
+      // if not ie < 9.
+      if(!window.console || !this.config.devMode) {
 
-			case 'info':
+        return this;
+      }
 
-				window.console.info(msg, ob || undefined);
-				break;
+      switch(lvl) {
 
-			case 'log':
+        case 'info':
 
-				window.console.log(msg, ob || undefined);
-				break;
+            window.console.info(msg, ob || undefined);
+            break;
 
-			case 'warn':
+        case 'log':
 
-				window.console.warn(msg, ob || undefined);
-				break;
+            window.console.log(msg, ob || undefined);
+            break;
 
-			case 'error':
+        case 'warn':
 
-				window.console.error(msg, ob || undefined);
-				window.console.log("+++ Start Stack Trace +++");
-				window.console.trace();
-				window.console.log("+++ End Stack Trace +++");
-				break;
+            window.console.warn(msg, ob || undefined);
+            break;
 
-			default:
+        case 'error':
 
-				window.console.error('DOH! BLOX.dbug passed unavailable level [%s]. Try "info", "log", "warn" or "error"', lvl);
-				break;
-			}
+            window.console.error(msg, ob || undefined);
+            window.console.log("+++ Start Stack Trace +++");
+            window.console.trace();
+            window.console.log("+++ End Stack Trace +++");
+            break;
 
-			return this;
+        default:
 
-		}
+            window.console.error('DOH! BLOX.dbug passed unavailable level [%s]. Try "info", "log", "warn" or "error"', lvl);
+            break;
+      }
 
-	};
+      return this;
 
-	window.BLOX = Blox;
+    }
+
+  };
+
+  window.BLOX = Blox;
 }());
